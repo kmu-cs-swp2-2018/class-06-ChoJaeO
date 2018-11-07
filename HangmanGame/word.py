03a4fae32da600976e2c439aebf005e263f91de0
JaeoCho <cho5095@kookmin.ac.kr>
2018-11-07 17:21:57 +0900

    행맨 게임 코드 작성해보기 과제

--- HangmanGame/word.py
+++ HangmanGame/word.py
@@ -1,7 +1,18 @@
+import random
+
 class Word:
     def __init__(self):
-        f = open("")
-        pass
+        self.word_list = []
+        # 파일 입력
+        f = open("words.txt")
+        words = f.readlines()
+        f.close()
+        for word in words:
+            # 빈칸 제거 작업
+            self.word_list.append(word.rstrip())
 
     def readFromDB(self):
-        pass
+        # 단어 하나 난수생성을 통해 선택
+        r = random.randrange(len(self.word_list))
+        # 단어 리턴
+        return self.word_list[r]
