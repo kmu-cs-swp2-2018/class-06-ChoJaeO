b2ae52b02a5e3718716e59acd639050b4da86ead
JaeoCho <cho5095@kookmin.ac.kr>
2018-11-12 07:34:25 +0900

    전체적인 게임 구성

--- HangmanGame/game.py
+++ HangmanGame/game.py
@@ -11,11 +11,14 @@ def gameMain():
     hangman = Hangman()
     maxTries = hangman.getLife()
 
+    try_cnt = 0
+
     while guess.numTries < maxTries:
         display = hangman.get(maxTries - guess.numTries)
+        print(display)
         guess.display()
-
         guessedChar = input("Select a letter : ")
+        print("-------------------------------")
         if len(guessedChar) != 1:
             # 두개 이상의 알파벳을 입력했을 때
             print("One character at a time!")
@@ -26,9 +29,12 @@ def gameMain():
             continue
         # guess.guess를 통해 최종 성공 여부를 반환
         finished = guess.guess(guessedChar)
-
+        try_cnt += 1
+        print("Tries : " + str(guess.numTries))
         if finished == True:
             break
+
+    print("-------------------------------")
     if maxTries > guess.numTries:
         print("Success")
 
@@ -36,7 +42,7 @@ def gameMain():
         # 루프가 다 돌고 완전히 실패했을 경우
         print(hangman.get(0))
         print("Word is "+guess.answer)
-        print("Guess" + guess.currentStatus)
+        print("Guess : " + guess.currentStatus)
         print("Fail")
 
 if __name__ == '__main__':
--- HangmanGame/guess.py
+++ HangmanGame/guess.py
@@ -1,6 +1,7 @@
 class Guess:
     def __init__(self,word):
         self.guess_list = []
+        self.currentStatus = ""
         self.answer = word
         self.numTries = 0
         self.guessedChars = ""
@@ -17,13 +18,16 @@ class Guess:
             if character == self.answer[i]:
                 self.guess_list[i] = 1
         print("Current : " , end ='')
+        self.currentStatus = ""
         for i in range(len(self.answer)):
             if self.guess_list[i] == 1:
+                self.currentStatus += self.answer[i]
                 print(self.answer[i], end ='')
             else:
+                self.currentStatus += "_"
                 print("_", end = '')
         print()
-
+        self.numTries += 1
         if 0 not in self.guess_list:
             return True
         else:
