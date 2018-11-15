269906a267f5290dfdb2db2d447aa84c5cc544fa
JaeoCho <cho5095@kookmin.ac.kr>
2018-11-15 22:16:23 +0900

    코드 리뷰 후 수정, for문 합치기

--- HangmanGame/guess.py
+++ HangmanGame/guess.py
@@ -5,7 +5,7 @@ class Guess:
         self.answer = word
         self.numTries = 0
         self.guessedChars = ""
-        for i in range(len(word)):
+        for i in word:
             self.guess_list.append(0)
 
     def display(self):
@@ -15,19 +15,19 @@ class Guess:
 
     def guess(self,character):
         self.guessedChars += character
-        for i in range(len(self.answer)):
-            if character == self.answer[i]:
-                self.guess_list[i] = 1
-        print("Current : " , end ='')
-        self.currentStatus = ""
         if character not in self.answer:
             self.numTries += 1
-        for i in range(len(self.answer)):
-            if self.guess_list[i] == 1:
-                self.currentStatus += self.answer[i] + " "
-                print(self.answer[i], end =' ')
-            else:
-                self.currentStatus += "_ "
-                print("_", end = ' ')
-        print()
+        else:
+            print("Current : " , end ='')
+            self.currentStatus = ""
+            for i in range(len(self.answer)):
+                if character == self.answer[i]:
+                    self.guess_list[i] = 1
+                if self.guess_list[i] == 1:
+                    self.currentStatus += self.answer[i] + " "
+                    print(self.answer[i], end =' ')
+                else:
+                    self.currentStatus += "_ "
+                    print("_", end = ' ')
+            print()
         return 0 not in self.guess_list
